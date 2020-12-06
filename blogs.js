1c9c7c4b5cdd600e6ab0944b077ae1d044bab6a6
rsolanki3 <rsolanki2506@gmail.com>
2020-12-05 21:07:47 -0500

    Add new js files and functions

--- blogs.js
+++ blogs.js
@@ -1,25 +1,3 @@
-const right = document.getElementById("right");
-var currentSlide = 0;
-var slideTrack = document.getElementById("slide-track");
-
-right.addEventListener("click", function(){
-  if (currentSlide < 5) {
-  currentSlide ++}
-  else {currentSlide =0;}
-  var translateAmount = currentSlide * 16.66
-  slideTrack.style.transform = `translateX(-${translateAmount}%)`
-})
-
-const left = document.getElementById("left")
-
-left.addEventListener("click", function(){
-  if (currentSlide >0) {
-  currentSlide-- }
-  else {currentSlide = 5;}
-  var translateAmount = currentSlide * 16.66
-  slideTrack.style.transform = `translateX(-${translateAmount}%)`
-})
-
 const redirectHome = document.querySelector("#home-tab");
 redirectHome.addEventListener("click", function(){
   window.location.href = "https://rsolanki3.github.io";
